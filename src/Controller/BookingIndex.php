c4a7a72f74afcbdd2afb5c229a0f6d3d27c8869a
erkosakkos <erko.sakkos@eesti.ee>
2020-11-03 13:21:51 +0200

    Read data from database and create view

--- src/Controller/BookingIndex.php
+++ src/Controller/BookingIndex.php
@@ -113,6 +113,9 @@ class BookingIndex extends AbstractController
      */
     public function bookings()
     {
-        return $this->render('bookings/bookings.html.twig');
+        $this->generateUrl('bookings');
+        $repository = $this->getDoctrine()->getRepository(Booking::class);
+        $bookings = $repository->findAll();
+        return $this->render('bookings/list.html.twig', ['bookings' => $bookings]);
     }
 }

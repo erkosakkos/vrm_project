206a6322c871c2464f2823d4a716ce61fd40cfa7
erkosakkos <erko.sakkos@eesti.ee>
2020-11-10 09:28:14 +0200

    Added menu for moving between routes

--- src/Controller/BookingIndex.php
+++ src/Controller/BookingIndex.php
@@ -25,10 +25,11 @@ use App\Entity\Booking;
 class BookingIndex extends AbstractController
 {
     /**
-     * @Route("/create_booking")
+     * @Route("/create_booking", name="create_booking")
      */
     public function create_booking(Request $request)
     {
+        $this->generateUrl('create_booking');
         $form = $this->createFormBuilder()
             ->add('firstName', TextType::class, [
                 'required' => true
@@ -69,7 +70,8 @@ class BookingIndex extends AbstractController
                 'required' => true
             ])
             ->add('additionalInformation', TextareaType::class, [
-                'required' => false
+                'required' => false,
+                'empty_data' => 'no additional information'
             ])
             ->add('submit', SubmitType::class, ['label' => 'Create booking'])
             ->getForm();
